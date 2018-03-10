464dcd8a1aa74610802fc546ac07a5a7a83299de
Lars Vidar <larsvidar@larsvidar.no>
2018-03-10 22:12:30 +0100

    - Made new methods for enabeling and disabeling buttons. - Renamed some variables. - Adjusted the layout.

--- app/src/main/java/no/larsvidar/badmintonscorekeeper/MainActivity.java
+++ app/src/main/java/no/larsvidar/badmintonscorekeeper/MainActivity.java
@@ -1,114 +1,161 @@
 package no.larsvidar.badmintonscorekeeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    /***** VARIABLES *****/
-    int teamAScore = 0;
-    int teamBScore = 0;
-    int teamASet = 0;
-    int teamBSet = 0;
+    /********** VARIABLES **********/
+    //Score variables
+    int teamAPoints = 0;
+    int teamBPoints = 0;
+    int teamAGames = 0;
+    int teamBGames = 0;
+    //View variables
+    Button teamAPointButton;
+    Button teamBPointButton;
+    Button teamAGameButton;
+    Button teamBGameButton;
+    TextView teamAPointsView;
+    TextView teamAGamesView;
+    TextView teamBPointsView;
+    TextView teamBGamesView;
 
-    /***** METHODS *****/
-    //Method for updating scores and sets.
+
+    /********** METHODS **********/
+
+    /**
+     * Method for updating scores and sets.
+     */
     private void updateScore() {
         //Updating Team A's score.
-        TextView teamAScoreView = findViewById(R.id.team_a_score);
-        teamAScoreView.setText(String.valueOf(teamAScore));
+        teamAPointsView.setText(String.valueOf(teamAPoints));
         //Updating Team A's set.
-        TextView teamASetView = findViewById(R.id.team_a_set);
-        teamASetView.setText(String.valueOf(teamASet));
+        teamAGamesView.setText(String.valueOf(teamAGames));
         //Updating Team B's score.
-        TextView teamBScoreView = findViewById(R.id.team_b_score);
-        teamBScoreView.setText(String.valueOf(teamBScore));
+        teamBPointsView.setText(String.valueOf(teamBPoints));
         //Updating Team B's score.
-        TextView teamBSetView = findViewById(R.id.team_b_set);
-        teamBSetView.setText(String.valueOf(teamBSet));
+        teamBGamesView.setText(String.valueOf(teamBGames));
     }
 
-
-    /***** BUTTON EVENTS *****/
-    public void setScoreA(View view) {
-        TextView teamAScoreButton = findViewById(R.id.score_a_button);
-        TextView teamASetButton = findViewById(R.id.set_a_button);
-        teamAScore++;
-        if (teamAScore >= 21 && teamAScore > teamBScore + 1) {
-            teamAScoreButton.setEnabled(false);
-            teamASetButton.setEnabled(true);
-        } else {
-            teamAScoreButton.setEnabled(true);
-            teamASetButton.setEnabled(false);
-        }
-        updateScore();
-    }
-
-    public void setScoreB(View view) {
-        TextView teamBScoreButton = findViewById(R.id.score_b_button);
-        TextView teamBSetButton = findViewById(R.id.set_b_button);
-        teamBScore++;
-        if (teamBScore >= 21 && teamBScore > teamAScore + 1) {
-            teamBScoreButton.setEnabled(false);
-            teamBSetButton.setEnabled(true);
-        } else {
-            teamBScoreButton.setEnabled(true);
-            teamBSetButton.setEnabled(false);
-        }
-        updateScore();
-    }
-
-    public void setSetA(View view) {
-        teamASet++;
-        setScoreToZero();
-        setButtonsToDefault();
-        updateScore();
-    }
-
-    public void setSetB(View view) {
-        teamBSet++;
-        setScoreToZero();
-        setButtonsToDefault();
-        updateScore();
-    }
-
-    public void reset(View view) {
-        teamASet = 0;
-        teamBSet = 0;
-        setScoreToZero();
-        setButtonsToDefault();
-        updateScore();
-    }
-
+    /**
+     * Method for setting scores to zero.
+     */
     private void setScoreToZero() {
-        teamAScore = 0;
-        teamBScore = 0;
+        teamAPoints = 0;
+        teamBPoints = 0;
     }
 
+    /**
+     * Method for setting buttons to their default state
+     */
     private void setButtonsToDefault() {
-        TextView teamAScoreButton = findViewById(R.id.score_a_button);
-        TextView teamBScoreButton = findViewById(R.id.score_b_button);
-        TextView teamASetButton = findViewById(R.id.set_a_button);
-        TextView teamBSetButton = findViewById(R.id.set_b_button);
-        teamAScoreButton.setEnabled(true);
-        teamBScoreButton.setEnabled(true);
-        teamASetButton.setEnabled(false);
-        teamBSetButton.setEnabled(false);
+        enableButton(teamAPointButton);
+        enableButton(teamBPointButton);
+        disableButton(teamAGameButton);
+        disableButton(teamBGameButton);
     }
 
+    private void disableButton(Button button) {
+        button.setEnabled(false);
+        button.setBackgroundColor(getResources().getColor(R.color.main_button_disabled_color));
+        button.setTextColor(getResources().getColor(R.color.disabled_button_text));
+    }
+
+    private void enableButton(Button button) {
+        button.setEnabled(true);
+        button.setBackgroundColor(getResources().getColor(R.color.main_button_color));
+        button.setTextColor(getResources().getColor(R.color.light_text_color));
+    }
+
+
+    /********** BUTTON EVENTS **********/
+
+    /**
+     * Method for increasing score for Team A
+     */
+    public void setScoreA(View view) {
+        teamAPoints++;
+        if (teamAPoints >= 21 && teamAPoints > teamBPoints + 1) {
+            disableButton(teamAPointButton);
+            enableButton(teamAGameButton);
+        } else {
+            enableButton(teamAPointButton);
+            disableButton(teamAGameButton);
+        }
+        updateScore();
+    }
+
+    /**
+     * Method for increasing score for Team B
+     */
+    public void setScoreB(View view) {
+        teamBPoints++;
+        if (teamBPoints >= 21 && teamBPoints > teamAPoints + 1) {
+            disableButton(teamBPointButton);
+            enableButton(teamBGameButton);
+        } else {
+            enableButton(teamBPointButton);
+            disableButton(teamBGameButton);
+        }
+        updateScore();
+    }
+
+    /**
+     * Method for incresing sets for Team A
+     */
+    public void setSetA(View view) {
+        teamAGames++;
+        setScoreToZero();
+        setButtonsToDefault();
+        updateScore();
+    }
+
+    /**
+     * Method for increasing sets for Team B
+     */
+    public void setSetB(View view) {
+        teamBGames++;
+        setScoreToZero();
+        setButtonsToDefault();
+        updateScore();
+    }
+
+    /**
+     * Method for resetting all scores.
+     */
+    public void reset(View view) {
+        teamAGames = 0;
+        teamBGames = 0;
+        setScoreToZero();
+        setButtonsToDefault();
+        updateScore();
+    }
+
+    /**
+     * onCreate function
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         /*** App starts here! ***/
-        //Disable Set set buttons.
-        TextView teamASetButton = findViewById(R.id.set_a_button);
-        teamASetButton.setEnabled(false);
-        TextView teamBSetButton = findViewById(R.id.set_b_button);
-        teamBSetButton.setEnabled(false);
+        // Setting view variables.
+        teamAPointButton = findViewById(R.id.point_a_button);
+        teamBPointButton = findViewById(R.id.point_b_button);
+        teamAGameButton = findViewById(R.id.game_a_button);
+        teamBGameButton = findViewById(R.id.game_b_button);
+        teamAPointsView = findViewById(R.id.team_a_score);
+        teamAGamesView = findViewById(R.id.team_a_set);
+        teamBPointsView = findViewById(R.id.team_b_score);
+        teamBGamesView = findViewById(R.id.team_b_set);
+
+        //set buttons to default states
+        setButtonsToDefault();
     }
 }
