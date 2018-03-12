29f47ec6ab63ecb1fd2f12c3507c74c25e2e9cdb
Lars Vidar <larsvidar@larsvidar.no>
2018-03-12 11:05:10 +0100

    - Cleaned up code and comments.

--- app/src/main/java/no/larsvidar/badmintonscorekeeper/MainActivity.java
+++ app/src/main/java/no/larsvidar/badmintonscorekeeper/MainActivity.java
@@ -2,7 +2,6 @@ package no.larsvidar.badmintonscorekeeper;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,21 +10,21 @@ public class MainActivity extends AppCompatActivity {
 
     /********** VARIABLES **********/
     //Score variables
-    private int teamAPoints = 0;
-    private int teamBPoints = 0;
-    private int teamAGames = 0;
-    private int teamBGames = 0;
+    private int teamAPoints = 0;    //Points for Team A
+    private int teamBPoints = 0;    //Points for Team B
+    private int teamAGames = 0;     //Games for Team A
+    private int teamBGames = 0;     //Games for Team B
     //View elements
-    private Button teamAPointButton;
-    private Button teamBPointButton;
-    private Button teamAGameButton;
-    private Button teamBGameButton;
-    private TextView teamAPointsView;
-    private TextView teamAGamesView;
-    private TextView teamBPointsView;
-    private TextView teamBGamesView;
-    private TextView gameWonMessage;
-    private TextView matchWonMessage;
+    private Button teamAPointButton;    //Button for giving points to Team A
+    private Button teamBPointButton;    //Button for giving points to Team B
+    private Button teamAGameButton;     //Button for giving games to Team A
+    private Button teamBGameButton;     //Button for giving games to Team B
+    private TextView teamAPointsView;   //TextView for showing Team A's points
+    private TextView teamAGamesView;    //TextView for showing Team A's games
+    private TextView teamBPointsView;   //TextView for showing Team B's points
+    private TextView teamBGamesView;    //TextView for showing Team B's games
+    private TextView gameWonMessage;    //TextView for showing Game Won message
+    private TextView matchWonMessage;   //TextView for showing Match Won message
 
 
     /********** METHODS **********/
@@ -56,14 +55,17 @@ public class MainActivity extends AppCompatActivity {
      * Method for setting buttons to their default state
      */
     private void setButtonsToDefault() {
+        //Enabling point-buttons.
         enableButton(teamAPointButton);
         enableButton(teamBPointButton);
+        //Disabling game-buttons.
         disableButton(teamAGameButton);
         disableButton(teamBGameButton);
     }
 
     /**
      * Method for disabeling buttons.
+     *
      * @param button to be disabled.
      */
     private void disableButton(Button button) {
@@ -74,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Method for enabeling buttons.
+     *
      * @param button to be enabled.
      */
     private void enableButton(Button button) {
@@ -84,45 +87,56 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Method for showing Game Won message.
+     *
      * @param team that won the game.
      */
     private void showGameWinner(String team) {
+        //Removing point-buttons while message is showing.
         teamAPointButton.setVisibility(View.GONE);
         teamBPointButton.setVisibility(View.GONE);
+        //CHecking which team won.
         if (team.equals("A")) {
             gameWonMessage.setText(R.string.team_a_won_game);
         } else {
             gameWonMessage.setText(R.string.team_b_won_game);
         }
+        //Making the message visible.
         gameWonMessage.setVisibility(View.VISIBLE);
     }
 
     /**
      * Method for displaying Match Won message.
+     *
      * @param team that won the match.
      */
     private void showMatchWinner(String team) {
+        //Hiding all buttons.
         teamAPointButton.setVisibility(View.GONE);
         teamBPointButton.setVisibility(View.GONE);
         teamAGameButton.setVisibility(View.GONE);
         teamBGameButton.setVisibility(View.GONE);
+        //Checking which team won.
         if (team.equals("A")) {
             matchWonMessage.setText(R.string.team_a_won_match);
         } else {
             matchWonMessage.setText(R.string.team_b_won_match);
         }
+        //Making the message visible
         matchWonMessage.setVisibility(View.VISIBLE);
     }
 
     /**
      * Method for hiding game or match winning message.
+     *
      * @param message to be hidden.
      */
     private void hideWinner(TextView message) {
+        //Making all buttons visible again.
         teamAPointButton.setVisibility(View.VISIBLE);
         teamBPointButton.setVisibility(View.VISIBLE);
         teamAGameButton.setVisibility(View.VISIBLE);
         teamBGameButton.setVisibility(View.VISIBLE);
+        //Hiding whatever message was showing.
         message.setVisibility(View.GONE);
     }
 
@@ -139,18 +153,21 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
     }
 
+
     /********** BUTTON EVENTS **********/
 
     /**
      * Method for increasing score for Team A
      */
     public void setScoreA(View view) {
+        //Adding points to team A
         teamAPoints++;
+        //Checking weather Team A has 21 points or higher with a two point lead.
         if (teamAPoints >= 21 && teamAPoints > teamBPoints + 1) {
             disableButton(teamAPointButton);
             enableButton(teamAGameButton);
             showGameWinner("A");
-            } else {
+        } else {
             enableButton(teamAPointButton);
             disableButton(teamAGameButton);
         }
@@ -161,7 +178,9 @@ public class MainActivity extends AppCompatActivity {
      * Method for increasing score for Team B
      */
     public void setScoreB(View view) {
+        //Adding points to team B.
         teamBPoints++;
+        //Checking weather Team B has 21 points or higher with a two point lead.
         if (teamBPoints >= 21 && teamBPoints > teamAPoints + 1) {
             disableButton(teamBPointButton);
             enableButton(teamBGameButton);
@@ -177,11 +196,14 @@ public class MainActivity extends AppCompatActivity {
      * Method for incresing sets for Team A
      */
     public void setGameA(View view) {
+        //Adding a game to Team A.
         teamAGames++;
+        //Resetting points to zero for both teams.
         setScoreToZero();
         setButtonsToDefault();
         hideWinner(gameWonMessage);
         updateScore();
+        //Checking weather Team A has enough games to win the match.
         if (teamAGames > 1) {
             showMatchWinner("A");
         }
@@ -191,11 +213,14 @@ public class MainActivity extends AppCompatActivity {
      * Method for increasing sets for Team B
      */
     public void setGameB(View view) {
+        //Adding a game to team B.
         teamBGames++;
+        //Resetting points to zero for both teams.
         setScoreToZero();
         setButtonsToDefault();
         hideWinner(gameWonMessage);
         updateScore();
+        //Checking weather Team B has enough games to win the match.
         if (teamBGames > 1) {
             showMatchWinner("B");
         }
@@ -217,6 +242,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * onCreate method
+     *
      * @param savedInstanceState
      */
     @Override
@@ -242,17 +268,18 @@ public class MainActivity extends AppCompatActivity {
 
         //Get saved state after screen rotation
         if (savedInstanceState != null) {
+            //Getting teams games - 1.
             teamAGames = savedInstanceState.getInt("savedTeamAGames") - 1;
-            teamBGames = savedInstanceState.getInt("savedTeamBGames") -1;
+            teamBGames = savedInstanceState.getInt("savedTeamBGames") - 1;
+            //Giving teams one game.
             teamAGameButton.performClick();
             teamBGameButton.performClick();
-
+            //Getting teams points - 1.
             teamAPoints = savedInstanceState.getInt("savedTeamAPoints") - 1;
             teamBPoints = savedInstanceState.getInt("savedTeamBPoints") - 1;
+            //Giving each team one point.
             teamAPointButton.performClick();
             teamBPointButton.performClick();
-
-            savedInstanceState.clear();
         }
     }
 }
